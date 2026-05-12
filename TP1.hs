869039fe126ebb8d74f2b6427ec88414461aef47
A-Cabrera-I <cabrera.alvaro155@gmail.com>
2026-05-12 10:21:13 -0300

    Comentarios del punto 2 y 3

--- TP1.hs
+++ TP1.hs
@@ -1,4 +1,4 @@
-import Data.List
+import Data.List --Para el sortBy del ej 2
 
 data NdTree p = Node (NdTree p) p (NdTree p) Int | Empty
               deriving (Eq, Ord, Show) 
@@ -8,9 +8,6 @@ class Punto p where
     dimension :: p -> Int -- devuelve el numero de coordenadas de un punto
     coord :: Int -> p -> Double -- devuelve la coordenada kesima de un punto (comenzando de 0)
     dist :: p -> p -> Double -- calcula la distancia entre dos puntos
-
---instance: si interpreto a Punto como un [Double], sus funcs se usan asi
---Esto no hace falta para hacer el resto de cosas, claramente
 
 -- 1A, GENERICA, USANDO coord Y dimension
 dist2 :: Punto p => p -> p -> Double
@@ -30,6 +27,7 @@ distancia3d (P3d(x1,x2,x3)) (P3d (y1,y2,y3)) = sqrt((y1 - x1)^2 + (y2 - x2)^2 + 
 
 ---- EJERCICIO 2 ----
 
+--Funcion generica, que toma un indice y dos puntos, y compara sus valores en ese indice
 compareidx :: Punto p => Int -> p -> p -> Ordering
 compareidx i p q = compare (coord i p) (coord i q)
 
@@ -39,18 +37,18 @@ compareidx i p q = compare (coord i p) (coord i q)
 
 fromList :: Punto p => [p] -> NdTree p
 fromList [] = Empty
-fromList puntos = aux puntos 0
+fromList puntos = aux puntos 0 --Defino aux, que nos ayuda a pasar el level actual (arranca en 0)
     where
-        aux [] _ = Empty
+        aux [] _ = Empty 
         aux pts level =
             let
-                d = dimension (head pts)
-                eje = level `mod` d
+                d = dimension (head pts) --La dimension de un punto es la misma que el resto
+                eje = level `mod` d 
 
                 --ORDENO PUNTOS
-                ptsordenados = sortBy (compareidx eje) pts
+                ptsordenados = sortBy (compareidx eje) pts --Ordeno pts segun compareidx en el eje actual
                 len = length ptsordenados
-                mid = len `div` 2
+                mid = len `div` 2 -- La mediana es el valor medio de una lista ordenada
 
                 --DIVIDO EN l Y R
                 izq = take mid ptsordenados --toma mid puntos
@@ -59,13 +57,12 @@ fromList puntos = aux puntos 0
             in Node (aux izq (level + 1)) raiz (aux der (level + 1)) eje
 
 {-PROBLEMAS DE fromList
-No esta chequeado el uso de izq y der si divide bien
-creo que !! y sortBy solo toma listas
+Complejidad O(nlog^2n), no es mala dentro de todo
 -}
 
 ---- EJERCICIO 3 ----
 insertar :: Punto p => p -> NdTree p -> NdTree p
-insertar p t = insertarRecu p t 0
+insertar p t = insertarRecu p t 0 --insertarRecu nos ayuda a pasar el level actual
   where
     insertarRecu punto Empty level = 
         Node Empty punto Empty (level `mod` dimension punto)
